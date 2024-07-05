93058d0a953695e78c3d8866b07bf27ebeef1722
Charl Fourie <charl.fourie@momentum.co.za>
2024-07-05 15:34:41 +0200

    Implementing initial configurability

--- src/features/journal-folder-settings/journal-folder-settings-tab.ts
+++ src/features/journal-folder-settings/journal-folder-settings-tab.ts
@@ -1,4 +1,4 @@
-import { type Plugin, PluginSettingTab, Setting } from 'obsidian'
+import { debounce, MomentFormatComponent, type Plugin, PluginSettingTab, Setting } from 'obsidian'
 import { DEFAULT_SETTINGS, type JournalFolderSettings } from '../../data-access'
 
 type SettingsStringFieldName = 'dailyNoteTitlePattern' | 'dailyNoteShortTitlePattern' | 'weeklyNoteTitlePattern'
@@ -15,34 +15,126 @@ export class JournalFolderSettingsTab extends PluginSettingTab {
 	}
 
 	display() {
+		this.containerEl.empty()
 		const settings = { ...this.getCurrentSettings() }
 
+		const headingEl = document.createElement('h1')
+		headingEl.innerText = 'Journal Folder Settings'
+		this.containerEl.appendChild(headingEl)
+
 		this.createMomentSetting(settings, 'dailyNoteTitlePattern', 'Daily note title pattern')
+			.setDesc(
+				"The pattern used to render the title of a daily note. " +
+				"This pattern should not render any date/time elements shorter than a day (e.g. hour or minute). " +
+				"For instance, using a pattern of 'WW-HH' would not make sense " +
+				"as the hour component represents a fraction of the day."
+			)
+
 		this.createMomentSetting(settings, 'dailyNoteShortTitlePattern', 'Daily note short title pattern')
+			.setDesc(
+				"The pattern used to render links to daily notes. The user should aim to keep this pattern short " +
+				"as multiple links may be rendered next to each other. " +
+				"This pattern should not render any date/time elements shorter than a day (e.g. hour or minute). " +
+				"For instance, using a pattern of 'WW-HH' would not make sense " +
+				"as the hour component represents a fraction of the day."
+			)
+
 		this.createMomentSetting(settings, 'weeklyNoteTitlePattern', 'Weekly note title pattern')
+			.setDesc(
+				"The pattern used to render the title of a weekly note. " +
+				"This pattern should not render any date/time elements shorter than a week (e.g. day or hour). " +
+				"For instance, using a pattern of 'WW-DD' would not make sense " +
+				"as the day component represents a fraction of the week."
+			)
+
 		this.createMomentSetting(settings, 'weeklyNoteShortTitlePattern', 'Weekly note short title pattern')
+			.setDesc(
+				"The pattern used to render links to weekly notes. The user should aim to keep this pattern short " +
+				"as multiple links may be rendered next to each other. " +
+				"This pattern should not render any date/time elements shorter than a week (e.g. day or hour). " +
+				"For instance, using a pattern of 'WW-DD' would not make sense " +
+				"as the day component represents a fraction of the week."
+			)
+
 		this.createMomentSetting(settings, 'monthlyNoteTitlePattern', 'Monthly note title pattern')
+			.setDesc(
+				"The pattern used to render the title of a monthly note. " +
+				"This pattern should not render any date/time elements shorter than a month (e.g. week or day). " +
+				"For instance, using a pattern of 'MM-DD' would not make sense " +
+				"as the day component represents a fraction of the month."
+			)
+
 		this.createMomentSetting(settings, 'monthlyNoteShortTitlePattern', 'Monthly note short title pattern')
+			.setDesc(
+				"The pattern used to render links to monthly notes. The user should aim to keep this pattern short " +
+				"as multiple links may be rendered next to each other. " +
+				"This pattern should not render any date/time elements shorter than a month (e.g. week or day). " +
+				"For instance, using a pattern of 'MM-DD' would not make sense " +
+				"as the day component represents a fraction of the month."
+			)
+
 		this.createMomentSetting(settings, 'yearlyNoteTitlePattern', 'Yearly note title pattern')
+			.setDesc(
+				"The pattern used to render the title of a yearly note. " +
+				"This pattern should not render any date/time elements shorter than a year (e.g. month, week or day). " +
+				"For instance, using a pattern of 'YYYY-MM' would not make sense " +
+				"as the month component represents a fraction of the year."
+			)
+
 		this.createMomentSetting(settings, 'yearlyNoteShortTitlePattern', 'Yearly note short title pattern')
+			.setDesc(
+				"The pattern used to render links to yearly notes. The user should aim to keep this pattern short " +
+				"as multiple links may be rendered next to each other. " +
+				"This pattern should not render any date/time elements shorter than a year (e.g. month, week or day). " +
+				"For instance, using a pattern of 'YYYY-MM' would not make sense " +
+				"as the month component represents a fraction of the year."
+			)
+
+		new Setting(this.containerEl)
+			.setName("Reset all to default values")
+			.addButton(btn => {
+				btn.setIcon('reset')
+					.setWarning()
+			})
 	}
 
 	createMomentSetting(settings: JournalFolderSettings, fieldName: SettingsStringFieldName, name: string): Setting {
-		const sampleEl = document.createElement('div')
+		let component: MomentFormatComponent
+		const sampleValueEl = document.createElement('span')
 
 		const setting = new Setting(this.containerEl)
 			.setName(name)
 			.addMomentFormat(text => {
+				component = text
+				const onChange = debounce(
+					(value: string) => {
+						settings[fieldName] = value
+						// noinspection JSIgnoredPromiseFromCall
+						this.saveSettings(settings)
+					}, 250, true,
+				)
+
 				text.setDefaultFormat(DEFAULT_SETTINGS[fieldName])
 				text.setValue(settings[fieldName])
-					// .setPlaceholder(DEFAULT_SETTINGS[fieldName])
-					.onChange(async (value) => {
-						settings[fieldName] = value
-						await this.saveSettings(settings)
+					.onChange(onChange)
+				text.setSampleEl(sampleValueEl)
+			})
+			.addExtraButton(btn => {
+				btn.setIcon('reset')
+					.setTooltip('Reset to default value')
+					.onClick(() => {
+						component.setValue(DEFAULT_SETTINGS[fieldName])
+						component.onChanged()
 					})
-				text.setSampleEl(sampleEl)
 			})
 
+		const sampleEl = document.createElement('div')
+		sampleEl.setAttr('style', 'margin-left: auto; width: fit-content; opacity: 50%; margin-bottom: .25em;')
+		const sampleLabelEl = document.createElement('span')
+		sampleLabelEl.setAttr('style', 'opacity: 50%')
+		sampleLabelEl.setText("Sample value: ")
+		sampleEl.appendChild(sampleLabelEl)
+		sampleEl.appendChild(sampleValueEl)
 		this.containerEl.appendChild(sampleEl)
 		return setting
 	}
